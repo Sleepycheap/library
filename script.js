d35258f727d49fbb0174fa7939dfbbf6265b24ae
Sleepycheap <anthony.vaut@gmail.com>
2025-09-13 20:51:54 -0500

    fixed library display bug

--- script.js
+++ script.js
@@ -1,53 +1,84 @@
 
 
 const myLibrary = [{
-  "title": "Fourth Wing",
-  "author": "Rebecca Yarros",
-  "genre": "Fantasy"
+  "title": "This is the first book",
+  "author": "guy one",
+  "genre": "sci-fi"
 }, {
-  "title": "Eragon",
-  "author": "Christopher Paolini",
-  "genre": "Fantasy"
-}, {
-  "title": "Twilight",
-  "author": "Stephanie Meyer",
-  "genre": "Fantasy"
+  "title": "this is the second book",
+  "author": "guy two",
+  "genre": "fantasy"
 }];
-function Book(title, author, genre) {
+
+const displayed = [];
+
+function Book(title, author, genre, id) {
   this.title = title;
   this.author = author;
   this.genre = genre;
+  this.id = id;
 }
 
-const addBookToLibrary = function(title, author, genre) {
-  const item = new Book(title, author, genre)
+const addBookToLibrary = function(title, author, genre, id) {
+  const item = new Book(title, author, genre, id)
   console.log(item);
   myLibrary.push(item);
 }
 
 
+
+//if item in displayed dont display
+
+
 const revealButton = document.body.querySelector('.reveal');
+
 const books = document.querySelector('.books');
+
+// const displayCard = (title, author, genre) => {
+//   newCont.appendChild(title);
+//   newCont.appendChild(author);
+//   newCont.appendChild(genre);
+// }
+const createDiv = (item) => {
+  const newCont = document.createElement('div');
+  newCont.classList.add('boxes');
+  books.appendChild(newCont)
+  const title = document.createElement('div');
+  title.classList.add('info')
+  title.textContent = `Title: ${item.title}`;
+  const author = document.createElement('div');
+  author.classList.add('info');
+  author.textContent = `Author: ${item.author}`;
+  const genre = document.createElement('div');
+  genre.classList.add('info');
+  genre.textContent = `Genre: ${item.genre}`;
+  newCont.appendChild(title);
+  newCont.appendChild(author);
+  newCont.appendChild(genre);
+}
+
+
 revealButton.addEventListener('click', (e) => {
   e.preventDefault();
-  for (item of myLibrary) {
-    const newCont = document.createElement('div');
-    newCont.classList.add('boxes');
-    books.appendChild(newCont)
-    const title = document.createElement('div');
-    title.classList.add('info')
-    title.textContent = `Title: ${item.title}`;
-    const author = document.createElement('div');
-    author.classList.add('info');
-    author.textContent = `Author: ${item.author}`;
-    const genre = document.createElement('div');
-    genre.classList.add('info');
-    genre.textContent = `Genre: ${item.genre}`;
-    newCont.appendChild(title);
-    newCont.appendChild(author);
-    newCont.appendChild(genre);
-  }
-})
+  const length = myLibrary.length;
+  console.log(`Displayed: ${displayed}`);
+    for (item of myLibrary){
+      console.log(`Title: ${item.title}`);
+      //console.log(displayed);
+      if (displayed.includes(`${item.title}`)) {
+        console.log(`${item.title} Already Displayed`);
+        continue;
+      }
+      else {
+        createDiv(item);
+        displayed.push(item.title);
+      }
+
+  }})
+
+
+
+
 
 
 
@@ -58,14 +89,11 @@ button.addEventListener('click', (e) => {
   const t = prompt('Book title');
   const a = prompt('author');
   const g = prompt('genre');
-  addBookToLibrary(t, a, g);
+  const id = crypto.randomUUID();
+  addBookToLibrary(t, a, g, id);
 });
 
-// function handleSubmit(e) {
-//   e.preventDefault;
-//   const formData = new Book(e.target);
-//   console.log(formData);
-// }
+//const  
 
 const input = document.querySelector('#submitButton');
 //input.addEventListener('submit', handleSubmit);
